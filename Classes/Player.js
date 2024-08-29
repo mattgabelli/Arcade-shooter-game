09faf53cf4a225586139962539702dbe4cd78ef7
Matt Gabelli <92337185+mgg3478GIT@users.noreply.github.com>
2024-08-29 16:18:43 +0100

    Update Player.js

--- Classes/Player.js
+++ Classes/Player.js
@@ -1,1 +1,41 @@
+/* Class file from which player is created this forms the backbone for the gameplay*/
+class Player {
+    constructor(startX, startY, colour) {
+        this.startX = startX;
+        this.startY = startY;
+        this.colour = colour;
+    }
 
+    draw() {
+        fill('black');
+        rect(myCar.getStartX(), myCar.getStartY()-10, 60, 10, 2); //wheels
+        rect(myCar.getStartX(), myCar.getStartY()+20, 60, 10, 2);
+        fill(myCar.getColour());
+        strokeWeight(2);
+        rect(myCar.getStartX(), myCar.getStartY(), 50, 80, 15);
+    }
+
+    getStartX() {
+        return this.startX;
+    }
+
+    setStartX(newX) {
+        this.startX = newX;
+    }
+
+    getStartY() {
+        return this.startY;
+    }
+
+    setStartY(newY) {
+        this.startY = newY;
+    }
+
+    getColour() {
+        return this.colour;
+    }
+
+    setColour(newColour) {
+        this.colour = newColour;
+    }
+}
